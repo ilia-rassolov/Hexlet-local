a20fe690054ca191ffc687e41947a8a6bee562cf
user <iliarassolov@gmail.com>
2023-11-14 23:05:52 +0400

    home all

--- 06.text-I-O-basics/merge/g_merge(file1, file2, out).py
+++ 06.text-I-O-basics/merge/g_merge(file1, file2, out).py
@@ -1,12 +1,15 @@
+
+
 def merge(file1, file2, out):
     with open(file1) as input_1:
         with open(file2) as input_2:
             with open(out, "w") as output_file:
-                for line1, line2 in zip(input_1, input_2):
+                for line1, line2, i in zip(input_1, input_2, range(1, 100)):
                     if line1 == line2:
+                        diff1 = False
                         output_file.write(line1)
                     else:
-                        output_file.write(f"'>>>file1>>>', '\n', {line1}, '\n', '=====', '\n'. {line2}, '>>>file2>>>', '\n'")
+                        output_file.write(f"'>>>file1>>>\n', {line1}, '=====\n'. {line2}, '>>>file2>>>\n'")
 
 
 merge('file_1.txt', 'file_2.txt', 'out_file.txt')

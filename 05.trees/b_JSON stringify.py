f3ed49c781bb54b7ef4375a6b7fabfb7d44028fd
ilia rassolov <iliarassolov@gmail.com>
2024-04-09 16:16:01 +0400

    office 04/09/2024

--- 05.trees/b_JSON stringify.py
+++ 05.trees/b_JSON stringify.py
@@ -1,4 +1,3 @@
-
 def stringify(data, replacer=' ', spaces_count=1):
     INDENT = replacer * spaces_count
 
@@ -18,26 +17,24 @@ def stringify(data, replacer=' ', spaces_count=1):
 
     return inner(data)
 
+import itertools
 
 
-
-def stringify(data, replacer=' ', spaces_count=1):
-    if not isinstance(data, dict):
-        return str(replacer)*int(spaces_count) + str(value)
-    else:
-        result = '{\n'
-        for key in data:
-            result += f'    {key}: {data1_str[key]}\n'
-
-
-                if key in data2_str:
-                    if data1_str[key] == data2_str[key]:
-
-                    else:
-                        result += (f'  - {key}: {data1_str[key]}\n  + '
-                                   f'{key}: {data2_str[key]}\n')
-                else:
-                    result += f'  - {key}: {data1_str[key]}\n'
-            else:
-                result += f'  + {key}: {data2_str[key]}\n'
-        result += '}'
+# # BEGIN
+# def stringify(value, replacer=' ', spaces_count=1):
+#
+#     def iter_(current_value, depth):
+#         if not isinstance(current_value, dict):
+#             return str(current_value)
+#
+#         deep_indent_size = depth + spaces_count
+#         deep_indent = replacer * deep_indent_size
+#         current_indent = replacer * depth
+#         lines = []
+#         for key, val in current_value.items():
+#             lines.append(f'{deep_indent}{key}: {iter_(val, deep_indent_size)}')
+#         result = itertools.chain("{", lines, [current_indent + "}"])
+#         return '\n'.join(result)
+#
+#     return iter_(value, 0)
+# # END
